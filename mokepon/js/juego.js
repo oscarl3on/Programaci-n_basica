d5b7cb249ec5fbef11417bd22331d91d9d5be33c
oscarl3on <oscarleon.alcatel@gmail.com>
2022-08-29 18:39:43 -0600

    algoritmo seleccionar

--- mokepon/js/juego.js
+++ mokepon/js/juego.js
@@ -1,6 +1,27 @@
-function seleccionarMascotaJugador(){
-    alert('Seleccionaste tu mascota')
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+    let inpuntHipodoge = document.getElementById('hipodoge')
+    let inpuntCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let inputTucapalma = document.getElementById('tucapalma')
+    let inputPydos = document.getElementById('pydos')
+
+    if (inpuntHipodoge.checked){
+        alert("Seleccionaste a Hipodoge")
+    } else if (inpuntCapipepo.checked){
+        alert("Seleccionaste a Capipepo")
+    } else if (inputRatigueya.checked){
+        alert("Selecionaste a Ratigueya")
+    } else if (inputTucapalma.checked){
+        alert("Seleccionaste a Tucapalma")
+    } else if (inputPydos.checked){
+        alert("Seleccionaste a Pydos")
+    }
+}
+
+
+window.addEventListener('load', iniciarJuego)
