0516d5769a0219c0179f51d42e4a572806531013
oscarl3on <oscarleon.alcatel@gmail.com>
2022-09-02 19:13:48 -0600

    se corrige el error en langsotelvis

--- 03_Mokepon/js/juego.js
+++ 03_Mokepon/js/juego.js
@@ -7,6 +7,7 @@ function seleccionarMascotaJugador(){
     let inpuntHipodoge = document.getElementById('hipodoge')
     let inpuntCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
+    let inputLangostelvis = document.getElementById('langostelvis')
     let inputTucapalma = document.getElementById('tucapalma')
     let inputPydos = document.getElementById('pydos')
     let spandMascotaJugador = document.getElementById('mascota-jugador')
@@ -17,7 +18,9 @@ function seleccionarMascotaJugador(){
         spandMascotaJugador.innerHTML = 'Capipepo'
     } else if (inputRatigueya.checked){
         spandMascotaJugador.innerHTML = 'Ratigueya'
-    } else if (inputTucapalma.checked){
+    }else if (inputLangostelvis.checked){
+        spandMascotaJugador.innerHTML = 'Langostelvis'
+    }else if (inputTucapalma.checked){
         spandMascotaJugador.innerHTML = 'Tucapalma'
     } else if (inputPydos.checked){
         spandMascotaJugador.innerHTML = 'Pydos'
