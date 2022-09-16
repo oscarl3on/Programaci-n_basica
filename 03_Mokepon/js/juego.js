dbc957b6a2fe7c69a59240e3b3f3ef27d33ff1ef
oscarl3on <oscarleon.alcatel@gmail.com>
2022-09-16 12:29:00 -0600

    ataque jugadores

--- 03_Mokepon/js/juego.js
+++ 03_Mokepon/js/juego.js
@@ -1,12 +1,16 @@
 let ataqueJugador
+let ataqueEnemigo
 
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
-    let botonFuego
-    let botonAgua
-    let botonTierra
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMascotaJugador(){
@@ -39,21 +43,48 @@ seleccionarMascotaEnemigo()
 }
 
 function seleccionarMascotaEnemigo(){
-    let ataqueAleatorio = aleatorio(1,6)
+    let mascotaAleatoria = aleatorio(1,6)
     let spandMascotaEnemigo = document.getElementById('mascota-enemigo')
 
-    if (ataqueAleatorio == 1){
+    if (mascotaAleatoria == 1){
         spandMascotaEnemigo.innerHTML = 'Hipodoge'
-    } else if (ataqueAleatorio == 2){
+    } else if (mascotaAleatoria == 2){
         spandMascotaEnemigo.innerHTML = 'Capipepo'
-    } else if (ataqueAleatorio == 3){
+    } else if (mascotaAleatoria == 3){
         spandMascotaEnemigo.innerHTML = 'Ratigueya'
-    } else if (ataqueAleatorio == 4){
+    } else if (mascotaAleatoria == 4){
         spandMascotaEnemigo.innerHTML = 'Langostelvis'
-    } else if (ataqueAleatorio == 5){
+    } else if (mascotaAleatoria == 5){
         spandMascotaEnemigo.innerHTML = 'Tucapalma'
     } else {
         spandMascotaEnemigo.innerHTML = 'Pydos'
+    }
+}
+
+function ataqueFuego(){
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua(){
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueTierra(){
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    
+    if (ataqueAleatorio == 1){
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2){
+        ataqueEnemigo = 'AGUA'
+    } else{
+        ataqueEnemigo = 'TIERRA'
     }
 }
 
