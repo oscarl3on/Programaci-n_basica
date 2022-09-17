fcbfdc4135e34e9c42b901a8d243cddc6e17a4c8
oscarl3on <oscarleon.alcatel@gmail.com>
2022-09-17 11:13:33 -0600

    funciones para reiniciar el juego

--- 03_Mokepon/js/juego.js
+++ 03_Mokepon/js/juego.js
@@ -15,6 +15,9 @@ function iniciarJuego(){
     botonAgua.addEventListener('click', ataqueAgua)
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 
 function seleccionarMascotaJugador(){
@@ -150,6 +153,17 @@ function crearMensajeFinal(resultadoFinal){
     parrafo.innerHTML = resultadoFinal
 
     sectionMensajes.appendChild(parrafo)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true
+}
+
+function reiniciarJuego (){
+    location.reload()
 }
 
 function aleatorio(min, max){
