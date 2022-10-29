33fd081d53c4dd2cf1ed74613e342916aec8afc8
oscarl3on <oscarleon.alcatel@gmail.com>
2022-10-29 10:27:18 -0600

    movimiento continuo

--- 03_Mokepon/js/juego.js
+++ 03_Mokepon/js/juego.js
@@ -41,6 +41,7 @@ let victoriasEnemigo = 0
 let vidasJugador = 3
 let vidasEnemigo = 3
 let lienzo = mapa.getContext("2d")
+let intervalo
 
 class Mokepon {
     constructor(nombre, foto, vida){
@@ -54,6 +55,8 @@ class Mokepon {
         this.alto = 80
         this.mapaFoto = new Image()
         this.mapaFoto.src = foto
+        this.velocidadX = 0
+        this.velocidadY = 0
     }
 }
 
@@ -120,6 +123,7 @@ function seleccionarMascotaJugador() {
     sectionSeleccionarMascota.style.display = 'none'
     // sectionSeleccionarAtaque.style.display = 'flex'
     sectionVerMapa.style.display = 'flex'
+    intervalo = setInterval(pintarPersonaje, 50)
   
 
 
@@ -302,6 +306,8 @@ function aleatorio(min, max) {
 }
 
 function pintarPersonaje(){
+    capipepo.x = capipepo.x + capipepo.velocidadX
+    capipepo.y = capipepo.y + capipepo.velocidadY
     lienzo.clearRect(0, 0, mapa.width, mapa.height)
     lienzo.drawImage(
         capipepo.mapaFoto,
@@ -313,21 +319,21 @@ function pintarPersonaje(){
 }
 
 function moverDerecha(){
-    capipepo.x = capipepo.x + 5
-    pintarPersonaje()
+    capipepo.velocidadX = 5
 }
 function moverIzquierda(){
-    capipepo.x = capipepo.x - 5
-    pintarPersonaje()
+    capipepo.velocidadX = -5
 }
 function moverAbajo(){
-    capipepo.y = capipepo.y + 5
-    pintarPersonaje()
+    capipepo.velocidadY = 5
 }
 function moverArriba(){
-    capipepo.y = capipepo.y - 5
-    pintarPersonaje()
+    capipepo.velocidadY = -5
 }
 
+function detenerMovimiento(){
+    capipepo.velocidadX = 0
+    capipepo.velocidadY = 0
+}
 
 window.addEventListener('load', iniciarJuego)
