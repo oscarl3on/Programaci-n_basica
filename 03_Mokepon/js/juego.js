1eab2329bc466de9854f424e32c6b35b58dd0ac8
oscarl3on <oscarleon.alcatel@gmail.com>
2022-11-05 09:56:49 -0600

    nuevas teclas de movimiento

--- 03_Mokepon/js/juego.js
+++ 03_Mokepon/js/juego.js
@@ -46,25 +46,25 @@ let intervalo
 let mapaBackground = new Image()
 mapaBackground.src = './assets/mokemap.png'
 class Mokepon {
-    constructor(nombre, foto, vida){
+    constructor(nombre, foto, vida, fotoMapa, x = 10, y = 10){
         this.nombre = nombre
         this.foto = foto
         this.vida = vida
         this.ataques = []
         this.x = 20
         this.y = 20
-        this.ancho = 80
-        this.alto = 80
+        this.ancho = 40
+        this.alto = 40
         this.mapaFoto = new Image()
-        this.mapaFoto.src = foto
+        this.mapaFoto.src = fotoMapa
         this.velocidadX = 0
         this.velocidadY = 0
     }
 }
 
-let hipodoge = new Mokepon('Hipodoge','./assets/hipodoge.png', 5)
-let capipepo = new Mokepon('Capipepo','./assets/capipepo.png', 5)
-let ratigueya = new Mokepon('Ratigueya','./assets/ratigueya.png', 5)
+let hipodoge = new Mokepon('Hipodoge','./assets/hipodoge.png', 5,'./assets/hipodoge.png')
+let capipepo = new Mokepon('Capipepo','./assets/capipepo.png', 5,'./assets/capipepo.png')
+let ratigueya = new Mokepon('Ratigueya','./assets/ratigueya.png', 5,'./assets/ratigueya.png')
 
 hipodoge.ataques.push(
     {nombre: '💧', id: 'boton-agua'},
@@ -350,18 +350,22 @@ function sePresionoUnaTecla(event){
         // case 'ArrowUp':
         case 'u':
         case 'ArrowUp':
+        case 'w':
             moverArriba()
             break;
         case 'j':
         case 'ArrowDown':
+        case 's':
             moverAbajo()
             break;
         case 'h':
-        case 'ArrowLeft':    
+        case 'ArrowLeft': 
+        case 'a':   
             moverIzquierda()
             break;
         case 'k':
-        case 'ArrowRight':   
+        case 'ArrowRight':
+        case 'd':   
             moverDerecha()
             break;
         default:
