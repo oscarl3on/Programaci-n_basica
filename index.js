cf58d684e7014a4649589278739b857d5e1afc0c
oscarl3on <oscarleon.alcatel@gmail.com>
2022-11-12 09:52:13 -0600

    arrancando el servidor

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hola desde Node")
+// COMENTARIOS: iniciar proyecto con, npm init, crear el archivo index.js, crear servidor, arrancar servidor con node index.js, apagar con ctrl c.
+
+const express = require("express")
+const app = express()
+app.listen(8080, () => {
+    console.log("Servidor funcionando")
+})
